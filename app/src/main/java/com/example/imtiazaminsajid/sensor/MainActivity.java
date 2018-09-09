0d417070ebe3937a5aab3a16e16b688e32aff9be
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-09-09 23:58:47 +0600

    Sensor

--- app/src/main/java/com/example/imtiazaminsajid/sensor/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/sensor/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.imtiazaminsajid.sensor;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Sensor proximitySensor;
+    SensorEventListener eventListener;
+    SensorManager sensorManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
     }
 }
