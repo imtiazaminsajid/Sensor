ee6f549bc27c99f7b9b8a27bce8e5ef1660364a1
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-09-10 00:34:55 +0600

    Sensor

--- app/src/main/java/com/example/imtiazaminsajid/sensor/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/sensor/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.imtiazaminsajid.sensor;
 
+import android.graphics.Color;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
 
     Sensor proximitySensor;
     SensorEventListener eventListener;
@@ -16,7 +21,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView1);
+
+
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (proximitySensor==null){
+            finish();
+        }
+
+        eventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+
+                if (sensorEvent.values[0]<proximitySensor.getMaximumRange()){
+                    textView1.setBackgroundColor(Color.GREEN);
+                }
+                else {
+                    textView1.setBackgroundColor(Color.RED);
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(eventListener, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(eventListener);
+        super.onPause();
     }
 }
